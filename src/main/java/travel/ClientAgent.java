121e5b4e33827d7dc2a29d344bbc042da0ae1e5b
fmi <fmi@fmi.com>
2026-05-16 13:47:38 +0300

    End of the day commit

--- src/main/java/travel/ClientAgent.java
+++ src/main/java/travel/ClientAgent.java
@@ -1,7 +1,126 @@
 package travel;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import travel.gui.ClientGUI;
 
 public class ClientAgent extends Agent{
 
+	private ClientGUI gui;
+	private String searchedDestination;
+	private AID agency;	
+	
+	@Override
+	protected void setup() {
+		gui = new ClientGUI(this);
+	}
+	
+	public void searchDestination(String search) {
+		this.searchedDestination = search;
+		addBehaviour(travelQuest);
+	}
+	
+	private OneShotBehaviour travelQuest = new OneShotBehaviour() {
+		
+		@Override
+		public void action() {
+			DFAgentDescription da = new DFAgentDescription();
+			ServiceDescription sd = new ServiceDescription();
+			sd.setType("travel");
+			da.addServices(sd);
+			
+			try {
+				DFAgentDescription[] descrs = 
+						DFService.search(myAgent, da);
+				
+				if(descrs.length > 0) {
+					agency = descrs[0].getName();
+					addBehaviour(new SearchForDestination());
+				}else {
+					System.out.println("No agencies");
+				}
+				
+			} catch (FIPAException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	};
+
+	private class SearchForDestination extends Behaviour {
+
+		int step = 0;
+		MessageTemplate mt;
+		
+		@Override
+		public void action() {
+			switch(step) {
+				case 0:
+					ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+					cfp.addReceiver(agency);
+					cfp.setContent(searchedDestination);
+					cfp.setConversationId("slunce-more");
+					
+					mt = MessageTemplate.MatchConversationId("slunce-more");
+					
+					send(cfp);
+					step++;				
+				break;
+				case 1:
+					ACLMessage reply = receive(mt);
+					
+					if(reply == null) {
+						block();
+						return;
+					}
+					
+					if(reply.getPerformative() == ACLMessage.PROPOSE) {
+						ObjectMapper mapper = new ObjectMapper();
+						
+						try {
+							Destination result = mapper.readValue(
+									reply.getContent(), 
+									Destination.class);
+							gui.showResult(result.toString());
+													
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+					}else {
+						gui.showResult("We do not have this destination but we "
+								+ "have this to offer:"
+								+ reply.getContent());
+					}					
+					step++;				
+				break;
+				
+			}
+			
+		}
+
+		@Override
+		public boolean done() {
+			if(step == 2) return true;
+			
+			return false;
+		}
+		
+		
+	}
+	
 }
---
+++ src/main/java/travel/gui/ClientGUI.java
@@ -0,0 +1,71 @@
+package travel.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import travel.ClientAgent;
+
+public class ClientGUI extends JFrame{
+	
+	private JTextField searchField;
+	private JButton searchButton;
+	private JTextArea resultArea;
+	
+	private ClientAgent agent;
+
+	
+	
+	public ClientGUI(ClientAgent agent) {
+		this.agent = agent;
+		init();
+	}
+
+
+	private void init() {
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+
+		JLabel label = new JLabel("Search here:");
+		searchField = new JTextField();
+		searchButton = new JButton("Search");
+		resultArea = new JTextArea();
+		
+		panel.add(label);
+		panel.add(searchButton);
+		panel.add(searchField);
+		panel.add(resultArea);
+		add(panel);
+		
+		setSize(500, 700);
+		
+		label.setBounds(20,20,460,30);
+		searchField.setBounds(20, 70, 460, 30);
+		searchButton.setBounds(200, 120, 100, 30);
+		resultArea.setBounds(20,170,460,500);
+		
+		searchButton.addActionListener(searchClicked);
+		
+		setVisible(true);
+				
+	}
+	
+	private ActionListener searchClicked = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String search = searchField.getText();
+			agent.searchDestination(search);			
+		}
+	};
+	
+	public void showResult(String text) {
+		resultArea.setText(text);
+	}
+}
